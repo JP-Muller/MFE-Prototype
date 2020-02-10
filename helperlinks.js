a0c2d9dc24d3d9d012aa255527d86e783125b203
Jake M <mullerjp95@gmail.com>
2020-02-09 21:46:04 -0500

    added helpful links

---
+++ helperlinks.js
@@ -0,0 +1,11 @@
+https://gitlab.com/TheMcMurder/single-spa-portal-example/-/tree/master
+
+
+https://single-spa.js.org/docs/starting-from-scratch/
+
+https://single-spa.js.org/docs/getting-started-overview
+
+https://single-spa.js.org/docs/separating-applications/
+
+https://gitlab.com/TheMcMurder/single-spa-portal-example
+
